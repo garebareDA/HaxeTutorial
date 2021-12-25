a1caa14b9bf652714c52c95bf290af38317b844c
garebareDA <garedaiki@gmail.com>
2021-12-26 02:30:58 +0900

    Pikuups

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
 import haxe.Log;
 import flixel.FlxG;
 import flixel.tile.FlxTilemap;
@@ -13,19 +14,24 @@ class PlayState extends FlxState
 	var map:FlxOgmo3Loader;
 	var walls:FlxTilemap;
 
+	var coins:FlxTypedGroup<Coin>;
+
 	override public function create()
 	{
 		super.create();
 		map = new FlxOgmo3Loader(AssetPaths.turnBasedRPG__ogmo, AssetPaths.room_01__json);
 		walls = map.loadTilemap(AssetPaths.tiles__png, "walls");
-		walls.follow();
 		walls.setTileProperties(1, NONE);
 		walls.setTileProperties(2, ANY);
 		add(walls);
 
+		coins = new FlxTypedGroup();
+		add(coins);
+
 		player = new Player();
-		map.loadEntities(placeEntities, "entities");
 		add(player);
+
+		map.loadEntities(placeEntities, "entities");
 
 		FlxG.camera.follow(player, TOPDOWN, 1);
 	}
@@ -35,11 +41,20 @@ class PlayState extends FlxState
 		{
 			player.setPosition(entity.x, entity.y);
 		}
+		else if(entity.name == "coin")
+		{
+			coins.add(new Coin(entity.x + 4, entity.y + 4));
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		FlxG.overlap(player, coins, playerTouchCoin);
 		FlxG.collide(player, walls);
+	}
+
+	function playerTouchCoin(player:Player, coin:Coin) {
+		if(player.alive && player.exists && coin.alive && coin.exists) coin.kill();
 	}
 }
