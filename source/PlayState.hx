a84f3c15152e7ba836e1ca1f2dd36ee737aa40dd
garebareDA <garedaiki@gmail.com>
2021-12-26 01:48:54 +0900

    loading map

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,19 +1,43 @@
 package;
 
+import haxe.Log;
+import flixel.FlxG;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var player:Player;
+	var map:FlxOgmo3Loader;
+	var walls:FlxTilemap;
+
 	override public function create()
 	{
 		super.create();
-		var text = new FlxText(10, 10, 100, "Hello World!");
-		add(text);
+		map = new FlxOgmo3Loader(AssetPaths.turnBasedRPG__ogmo, AssetPaths.room_01__json);
+		walls = map.loadTilemap(AssetPaths.tiles__png, "walls");
+		walls.follow();
+		walls.setTileProperties(1, NONE);
+		walls.setTileProperties(2, ANY);
+		add(walls);
+
+		player = new Player();
+		map.loadEntities(placeEntities, "entities");
+		add(player);
+	}
+
+	function placeEntities(entity:EntityData) {
+		if (entity.name == "player")
+		{
+			player.setPosition(entity.x, entity.y);
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		FlxG.collide(player, walls);
 	}
 }
